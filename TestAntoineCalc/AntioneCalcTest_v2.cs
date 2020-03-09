c5995419972f785a7091fc47e974c272800d696c
Haobin Qu <hq617@york.ac.uk>
2020-03-09 23:52:51 +0000

    Added MSTest v2 using datarows for parameters

---
+++ TestAntoineCalc/AntioneCalcTest_v2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AntoineCalcApp;
+
+
+namespace TestAntoineCalc
+{
+    [TestClass]
+    public class AntioneCalcTest_v2
+    {
+        [TestMethod]
+        [DataRow(8.20417, 1642.89, 230.300, 78.32, 770, "Liquid")]
+        public void TestLiquidState1(double paraA, double paraB, double paraC, double currTemp, double currPressure, string expectState)
+        {
+            AntoineCalc Test1 = new AntoineCalc(paraA, paraB, paraC, currTemp, currPressure);
+
+            Test1.CalcPv();
+            string actualState = Test1.CalcState();
+
+            Assert.AreEqual(expectState, actualState);
+        }
+
+        [TestMethod]
+        [DataRow(8.20417, 1642.89, 230.300, 78.32, 760, "Gas")]
+        public void TestGasState1(double paraA, double paraB, double paraC, double currTemp, double currPressure, string expectState)
+        {
+            AntoineCalc Test1 = new AntoineCalc(paraA, paraB, paraC, currTemp, currPressure);
+
+            Test1.CalcPv();
+            string actualState = Test1.CalcState();
+
+            Assert.AreEqual(expectState, actualState);
+        }
+    }
+}
